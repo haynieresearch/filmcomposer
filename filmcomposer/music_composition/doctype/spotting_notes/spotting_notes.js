2eecfb1549107ed57bec97b8dca4a71ae16a580c
System Administrator [HRD] <sysadmin@ip-172-31-2-176.us-east-2.compute.internal>
2021-10-17 12:54:51 -0600

    updates

--- filmcomposer/music_composition/doctype/spotting_notes/spotting_notes.js
+++ filmcomposer/music_composition/doctype/spotting_notes/spotting_notes.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Lance Haynie, LLC and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Spotting Note', {
+frappe.ui.form.on('Spotting Notes', {
 	// refresh: function(frm) {
 
 	// }
--- filmcomposer/music_composition/doctype/spotting_notes/test_spotting_notes.py
+++ filmcomposer/music_composition/doctype/spotting_notes/test_spotting_notes.py
@@ -4,5 +4,5 @@
 # import frappe
 import unittest
 
-class TestSpottingNote(unittest.TestCase):
+class TestSpottingNotes(unittest.TestCase):
 	pass
