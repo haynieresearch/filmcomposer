166768fe0c13fb97e3ad319ae3b14bdbc7a77069
System Administrator [HRD] <sysadmin@ip-172-31-2-176.us-east-2.compute.internal>
2021-10-18 16:34:22 -0600

    updates

--- filmcomposer/music_composition/doctype/sketch_project/sketch_project.js
+++ filmcomposer/music_composition/doctype/sketch_project/sketch_project.js
@@ -1,12 +1,19 @@
 // Copyright (c) 2021, Lance Haynie, LLC and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Spotting Notes', {
+frappe.ui.form.on('Sketch Project', {
   setup: function(frm) {
     cur_frm.set_query("cue_id", "cues", function(doc, cdt, cdn) {
     	return{
     		filters: [
     			['Cue', 'production_id', '=', frm.doc.production_id]
+    		]
+    	}
+    });
+		cur_frm.set_query("spotting_notes_id", function(doc, cdt, cdn) {
+    	return{
+    		filters: [
+    			['Spotting Notes', 'production_id', '=', frm.doc.production_id]
     		]
     	}
     });
@@ -23,17 +30,17 @@ frappe.ui.form.on('Spotting Notes', {
     cur_frm.add_fetch('production_id', 'director_email', 'director_email')
     cur_frm.add_fetch('production_id', 'director_phone', 'director_phone')
     cur_frm.add_fetch('production_id', 'director_mobile', 'director_mobile')
-    frm.add_custom_button('Create Cue Sheet', () => {
-        frappe.new_doc('Cue Sheet', {
-            production_id: frm.doc.production_id,
-            spotting_notes_id: frm.doc.name,
-        })
-    })
-    frm.add_custom_button('Create Sketch Project', () => {
-        frappe.new_doc('Sketch Project', {
-            production_id: frm.doc.production_id,
-            spotting_notes_id: frm.doc.name,
-        })
-    })
+  },
+
+  "spotting_notes_id": function(frm) {
+      frappe.model.with_doc("Spotting Notes", frm.doc.spotting_notes_id, function() {
+          var tabletransfer= frappe.model.get_doc("Spotting Notes", frm.doc.spotting_notes_id)
+          $.each(tabletransfer.cues, function(index, row){
+              var d = frm.add_child("cues");
+              d.cue_id = row.cue_id;
+              d.cue_no = row.cue_no;
+              frm.refresh_field("cues");
+          });
+      });
   }
 });
