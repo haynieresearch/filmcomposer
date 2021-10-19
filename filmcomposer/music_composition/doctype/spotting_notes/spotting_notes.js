efc0a4e03b86da8829ce9e82a97be49a492bd172
Lance Haynie <lance@hayniemail.com>
2021-10-18 18:14:44 -0600

    updates

--- filmcomposer/music_composition/doctype/spotting_notes/spotting_notes.js
+++ filmcomposer/music_composition/doctype/spotting_notes/spotting_notes.js
@@ -1,6 +1,20 @@
-// Copyright (c) 2021, Lance Haynie, LLC and contributors
-// For license information, please see license.txt
-
+//**********************************************************
+//* CATEGORY	SOFTWARE
+//* GROUP	ERPNEXT/FRAPPE
+//* AUTHOR	LANCE HAYNIE
+//**********************************************************
+//Copyright Haynie IPHC, LLC
+//Developed by Haynie Research & Development, LLC
+//Developed for Lance Haynie, LLC
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.#
+//You may obtain a copy of the License at
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 frappe.ui.form.on('Spotting Notes', {
   setup: function(frm) {
     cur_frm.set_query("cue_id", "cues", function(doc, cdt, cdn) {
--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
+#**********************************************************
+#* CATEGORY	SOFTWARE
+#* GROUP	ERPNEXT/FRAPPE
+#* AUTHOR	LANCE HAYNIE
+#**********************************************************
+#Copyright Haynie IPHC, LLC
+#Developed by Haynie Research & Development, LLC
+#Developed for Lance Haynie, LLC
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.#
+#You may obtain a copy of the License at
+#http://www.apache.org/licenses/LICENSE-2.0
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in filmcomposer/__init__.py
 from filmcomposer import __version__ as version
 
 setup(
